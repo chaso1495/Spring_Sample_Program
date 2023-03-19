efac1adab2825f5a980ee25a0f4f9217eae99b27
CHAE <chaso1495@naver.com>
2023-03-19 15:20:15 +0900

    [feat] 도메인 추가

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,16 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Data // @Getter @Setter만 필요하면 이거 두개만 쓰는게 바람직하다
 public class Item {
-    private Long id;
-    private String itemName;
-    private Integer price;
-    private Integer quantity;
+
+    private Long id; // 상품 ID
+    private String itemName; // 상품명
+    private Integer price; // 가격
+    private Integer quantity; // 수량 (nullable을 위한 Integer)
 
     public Item() {
     }
